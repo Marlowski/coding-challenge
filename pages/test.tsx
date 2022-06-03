943fe50c2510b6167f91f30ffab13b2e2a96a59a
Marlowski <marlonkerth@icloud.com>
2022-06-03 14:28:25 +0200

    0.01 - added headlessui dependency

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,27 @@
+import { Transition } from '@headlessui/react'
+import { useState } from 'react'
+
+
+
 export default function Home() {
+    const [isShowing, setIsShowing] = useState(false);
+
     return (
-        <h1 className="text-3xl font-bold underline bg-amber-500 hover:bg-amber-200 transition-all">
-            Hello world!
-        </h1>
+        <>
+            <button onClick={() => setIsShowing((isShowing) => !isShowing)}>
+                Toggle
+            </button>
+            <Transition
+                show={isShowing}
+                enter="transition-opacity duration-75"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                I will fade in and out
+            </Transition>
+        </>
     )
 }
