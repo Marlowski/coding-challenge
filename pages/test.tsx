ebc60a654b27d2dba9a32e7fad84a16a183bb16e
Marlowski <marlonkerth@icloud.com>
2022-06-03 14:08:35 +0200

    inital commit

---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <h1 className="text-3xl font-bold underline bg-amber-500 hover:bg-amber-200 transition-all">
+            Hello world!
+        </h1>
+    )
+}
